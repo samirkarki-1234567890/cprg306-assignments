55349403f8a264be28a389d639d12b8e656e1ac7
samirkarki-1234567890 <samir.karki@edu.sait.ca>
2026-01-27 14:15:43 -0700

    week-3 files added

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+        <h1>CPRG 306: Web Development 2 - Assignments</h1>
+        <Link href="/week-2">
+        <button className="p-10 bg-gray-600 text-white rounded-lg" type="button">week-2</button></Link>
+        <Link href="/week-3">
+        <button className="p-10 bg-gray-600 text-white rounded-lg" type="button">week-3</button></Link>
+    </main>
+  );
+}
---
+++ app/week-2/student-info.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>SAMIR KARKI</h1>
+            <Link href="https://github.com/samirkarki-1234567890/cprg306-assignments" target="_blank" rel="noopener noreferrer">
+                <button type="button">github link</button>
+            </Link>
+        </main>
+    );
+}
